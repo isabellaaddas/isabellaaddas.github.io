c5e4aaec7fefc6bed0984d88c9fe2cf754d58034
isabellaaddas <isabellaaddas@gmail.com>
2025-09-11 13:40:03 -0400

    9/11 class

--- csce242/examples/04_js/js/intro.js
+++ csce242/examples/04_js/js/intro.js
@@ -8,6 +8,37 @@ const sayHello = () => {
 document.getElementById("btn-click-me").onclick = sayHello;
 */
 
-document.getElementById("btn-click-me").onclick = () => {
-    console.log("Hello World");
+document.getElementById("btn-click-me").onclick = (event) => {
+    /* Inner HTML refers to the HTML inside a tag */
+    document.getElementById("p-welcome").innerHTML = "Hello World";
+    // Current target refers to element that was clicked at event
+    event.currentTarget.classList.add("clicked");
+};
+
+document.getElementById("btn-happy").onclick = (event) => {
+    const pFeeling = document.getElementById("p-happy-sad-clear");
+    pFeeling.innerHTML = "YAY";
+    pFeeling.classList.add("happy-click");
+    pFeeling.classList.remove("sad-click");
+}
+
+document.getElementById("btn-sad").onclick = (event) => {
+    const pFeeling = document.getElementById("p-happy-sad-clear");
+    pFeeling.innerHTML = "NAY";
+    pFeeling.classList.add("sad-click");
+    pFeeling.classList.remove("happy-click");
+}
+
+document.getElementById("btn-clear").onclick = (event) => {
+    const pFeeling = document.getElementById("p-happy-sad-clear");
+    pFeeling.innerHTML = "";
+    pFeeling.classList.remove("sad-click");
+    pFeeling.classList.remove("happy-click");
+}
+
+document.getElementById("txt-emotion").onkeyup = (event) => {
+    const userInput = event.currentTarget.value;
+    // Back ticks evaluate the variables signified by ${}
+    document.getElementById("p-emotion").innerHTML = `You are feeling ${userInput}.`;
+    document.getElementById("img-emotion").classList.remove("hidden");
 };
---
+++ csce242/examples/04_js/js/toggle-nav.js
@@ -0,0 +1,4 @@
+document.getElementById("toggle-nav").onclick = () => {
+    console.log("clicked");
+    document.getElementById("nav-items").classList.toggle("hide-small");
+}
